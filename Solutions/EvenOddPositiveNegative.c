e85c7495bc0e60997d2c99c68faa369bdd13e4ed
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2025-10-14 21:24:41 +0600

    EvenOddPositiveNegative.c

--- Solutions/EvenOddPositiveNegative.c
+++ Solutions/EvenOddPositiveNegative.c
@@ -2,12 +2,12 @@
 #include <math.h>
 int main()
 {
-    int n,a,b,c,d,e;
+    int n,a,i;
     int even=0,odd=0,positive=0,negative=0;
     scanf("%d",&n);
-    scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
-    //for a
-    if(a%2==0){
+    for (i=1;i<=n;i++){
+        scanf("%d",&a);
+        if(a%2==0){
         even++;
     }else{
         odd++;
@@ -16,46 +16,9 @@ int main()
     }else if(a<0){
         negative++;
     }   
-    //for b
-    if(b%2==0){
-        even++;
-    }else{
-        odd++;
-    }if(b>0){
-        positive++;
-    }else if(b<0){
-        negative++;
-    }   
-    //for c
-    if(c%2==0){
-        even++;
-    }else{
-        odd++;
-    }if(c>0){
-        positive++;
-    }else if(c<0){
-        negative++;
-    }   
-    //for d
-    if(d%2==0){
-        even++;
-    }else{
-        odd++;
-    }if(d>0){
-        positive++;
-    }else if(d<0){
-        negative++;
-    }   
-    //for e
-    if(e%2==0){
-        even++;
-    }else{
-        odd++;
-    }if(e>0){
-        positive++;
-    }else if(e<0){
-        negative++;
-    }   
+    }
+    
+    
     
     printf("Even: %d\n",even);
     printf("Odd: %d\n",odd);
