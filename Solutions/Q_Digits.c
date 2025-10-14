55831a2d5ea6c14e0961a3190e8981f2f3df82a2
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2025-10-14 22:37:22 +0600

    Q_Digits.c

---
+++ Solutions/Q_Digits.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n,a,r,re,i,de;
+    scanf("%d",&n);
+    for (i=1;i<=n;i++){
+        scanf("%d",&a);
+        if (a==0){
+            printf("0\n");
+            continue;
+        }
+        for (;a!=0;){
+            re = a%10;
+            printf("%d ",re);
+            a = a/10;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+// problem link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/Q
