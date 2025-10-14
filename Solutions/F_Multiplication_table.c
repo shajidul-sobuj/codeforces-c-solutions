5a50b3ce1c97aa2ca9e9b5dd7669081f5d5c345b
shajidul islam Sobuj <143201902+shajidul-sobuj@users.noreply.github.com>
2025-10-14 21:34:55 +0600

    F_Multiplication_table.c

---
+++ Solutions/F_Multiplication_table.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,r;
+    scanf("%d",&n);
+    for (i=1;i<=12;i++){
+        printf("%d * %d = %d\n",n,i,n*i);
+    }
+    return 0;
+}
+
+
+
+// problem link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/F
